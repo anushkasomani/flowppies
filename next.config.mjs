6cca15d3f653626bedb037e1ac2fe84bdff6ee4b
anushkasomani <somanianu956@gmail.com>
2025-07-06 23:00:22 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Ignore TypeScript and ESLint errors
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   // Silence warnings for WalletConnect
   webpack: (config, context) => {
     if (config.plugins) {
@@ -11,6 +19,7 @@ const nextConfig = {
     }
     return config;
   },
+
   // Allow Pinata images (and add any other domains you need)
   images: {
     domains: ["gateway.pinata.cloud", "gateway.irys.xyz"],
